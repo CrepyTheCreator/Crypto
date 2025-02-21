5ddd490cf40434cd03e005b09d6c9dc0089111f3
Misha Zhuravlev <mishami816@gmail.com>
2025-02-21 18:58:45 +0300

    Stacking-add

--- scripts/script.js
+++ scripts/script.js
@@ -35,3 +35,48 @@ document.querySelectorAll(".input-content").forEach(container => {
       input.focus();
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector(".dropdown-btn");
+  const menu = document.querySelector(".dropdown-menu");
+  const dropdownIcon = document.querySelector(".drop-svg"); // Получаем элемент иконки
+
+  button.addEventListener("click", function () {
+    const isOpen = menu.style.display === "block";
+    menu.style.display = isOpen ? "none" : "block";
+    
+    // Добавляем или убираем класс drop-svg-open
+    dropdownIcon.classList.toggle("drop-svg-open", !isOpen);
+  });
+
+  document.addEventListener("click", function (event) {
+    if (!button.contains(event.target) && !menu.contains(event.target)) {
+      menu.style.display = "none";
+      dropdownIcon.classList.remove("drop-svg-open"); // Убираем класс при закрытии
+    }
+  });
+
+  menu.addEventListener("click", function (event) {
+    if (event.target.tagName === "LI") {
+      button.textContent = event.target.textContent;
+      menu.style.display = "none";
+      dropdownIcon.classList.remove("drop-svg-open"); // Убираем класс при закрытии
+    }
+  });
+});
+
+const slider = document.getElementById('stacking-range');
+const sliderValue = document.getElementById('range-value');
+const stackingValue = document.getElementById('stacking-value');
+const dropdownBtn = document.querySelector('.dropdown-btn');
+
+        function updateProgress() {
+            const percentage = (slider.value - slider.min) / (slider.max - slider.min) * 100 + '%';
+            slider.style.setProperty('--progress', percentage);
+            sliderValue.textContent = slider.value
+            const multiplier = dropdownBtn.textContent.trim() === 'TRX' ? 4 : 1;
+            stackingValue.value = slider.value * multiplier; // Обновляем значение в label
+        }
+
+        slider.addEventListener('input', updateProgress);
+        updateProgress(); // Инициализация
